82ff21c390eea70a74949794379e0c2c4817dd7a
mv-saini <snamvs75@gmail.com>
2025-11-22 20:04:25 +0100

    launch file for nodes

---
+++ src/launch_nodes/launch/system.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ros_tcp_endpoint',
+            executable='default_server_endpoint',
+            name='ros_tcp_endpoint',
+            parameters=[{
+                'ROS_IP': '0.0.0.0',
+                'ROS_TCP_PORT': 10000
+            }],
+            output='screen'
+        ),
+        
+        Node(
+            package='robot_manager',
+            executable='robot_manager_server.py',
+            name='robot_manager',
+            output='screen'
+        ),
+
+        Node(
+            package='path_planner',
+            executable='path_planner_server.py',
+            name='path_planner_server',
+            output='screen'
+        ),
+        
+        Node(
+            package='obstacle_manager',
+            executable='obstacle_manager_server.py',
+            name='obstacle_manager',
+            output='screen'
+        ),
+    ])
